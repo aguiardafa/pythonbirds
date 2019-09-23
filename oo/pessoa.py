977536a096927f812e8d1692463c331c944e5f16
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-09-23 18:43:10 -0300

    Adicionado método cumprimentar - referente ao estudo sobre a definção de métodos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,11 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(id(p))
+    print(Pessoa.cumprimentar(p))
+    # passagem da própria classe como parâmetro
+    # é feita implicitamente pelo python
+    print(p.cumprimentar())
