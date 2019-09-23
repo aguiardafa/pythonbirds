9c99bac7a8515bb421ef53efc658eca2f0ed6084
aguiardafa <diego.fernandes.aguiar@gmail.com>
2019-09-23 18:55:56 -0300

    Criado atribuitos de instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,19 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=30):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Diego')
     print(id(p))
     print(Pessoa.cumprimentar(p))
     # passagem da própria classe como parâmetro
     # é feita implicitamente pelo python
     print(p.cumprimentar())
+
+    print(p.nome)
+    p.nome = 'Aguiar'
+    print(p.nome)
